d284348c8ad6d0034e06f628a0c5732a7774f934
Arunkumar <127836192+ARUNKUMAR105@users.noreply.github.com>
2024-10-29 10:08:25 +0530

    Files added

---
+++ script.js
@@ -0,0 +1,9 @@
+function update(){
+    let name = document.getElementById("n1").value;
+    document . getElementById("n2").innerHTML = name;
+    let url = document.getElementById("p1").value;
+    document . getElementById("p2").src = url;
+    let about = document.getElementById("a1").value;
+    document . getElementById("a2").innerHTML = about;
+
+ }
